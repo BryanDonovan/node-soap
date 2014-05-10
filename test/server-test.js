d5a3ae37cd71f907d474777a590b0735c054ef40
Bryan Donovan <bdondo@gmail.com>
2014-05-10 12:28:53 -0700

    forking 0.1.2 of original npm lib

--- test/server-test.js
+++ test/server-test.js
@@ -1,139 +1,62 @@
-"use strict";
-
 var fs = require('fs'),
     soap = require('..'),
     assert = require('assert'),
     request = require('request'),
     http = require('http');
 
-var test = {};
-test.server = null;
-test.service = {
-  StockQuoteService: {
-    StockQuotePort: {
-      GetLastTradePrice: function(args) {
-        if (args.tickerSymbol === 'trigger error') {
-          throw new Error('triggered server error');
-        } else {
-          return { price: 19.56 };
+var service = { 
+    StockQuoteService: { 
+        StockQuotePort: { 
+            GetLastTradePrice: function(args) {
+                return { price: 19.56 };
+            }
         }
-      }
     }
-  }
-};
+}
 
-describe('SOAP Server', function() {
-  before(function(done) {
-    fs.readFile(__dirname + '/wsdl/strict/stockquote.wsdl', 'utf8', function(err, data) {
-      assert.ok(!err);
-      test.wsdl = data;
-      done();
-    });
-  });
+module.exports = {
+    'SOAP Server': {
 
-  beforeEach(function(done) {
-    test.server = http.createServer(function(req, res) {
-      res.statusCode = 404;
-      res.end();
-    });
+        'should start': function(done) {
+            var wsdl = fs.readFileSync(__dirname+'/stockquote.wsdl', 'utf8'),
+                server = http.createServer(function(req, res) {
+                    res.statusCode = 404;
+                    res.end();
+                });
+            server.listen(15099);
+            soap.listen(server, '/stockquote', service, wsdl);
+            request('http://localhost:15099', function(err, res, body) {
+                assert.ok(!err);
+                done();
+            })
+        },
 
-    test.server.listen(15099, null, null, function() {
-      test.soapServer = soap.listen(test.server, '/stockquote', test.service, test.wsdl);
-      test.baseUrl =
-        'http://' + test.server.address().address + ":" + test.server.address().port;
+        'should 404 on non-WSDL path': function(done) {
+            request('http://localhost:15099', function(err, res, body) {
+                assert.ok(!err);
+                assert.equal(res.statusCode, 404);
+                done();
+            })
+        },
 
-      //windows return 0.0.0.0 as address and that is not 
-      //valid to use in a request
-      if (test.server.address().address === '0.0.0.0') {
-        test.baseUrl =
-          'http://127.0.0.1:' + test.server.address().port;
-      }
+        'should server up WSDL': function(done) {
+            request('http://localhost:15099/stockquote?wsdl', function(err, res, body) {
+                assert.ok(!err);
+                assert.equal(res.statusCode, 200);
+                assert.ok(body.length);
+                done();
+            })            
+        },
 
-      done();
-    });
-  });
-
-  afterEach(function(done) {
-    test.server.close(function() {
-      test.server = null;
-      delete test.soapServer;
-      test.soapServer = null;
-      done();
-    });
-  });
-
-
-  it('should be running', function(done) {
-    request(test.baseUrl, function(err, res, body) {
-      assert.ok(!err);
-      done();
-    });
-  });
-
-  it('should 404 on non-WSDL path', function(done) {
-    request(test.baseUrl, function(err, res, body) {
-      assert.ok(!err);
-      assert.equal(res.statusCode, 404);
-      done();
-    });
-  });
-
-  it('should server up WSDL', function(done) {
-    request(test.baseUrl + '/stockquote?wsdl', function(err, res, body) {
-      assert.ok(!err);
-      assert.equal(res.statusCode, 200);
-      assert.ok(body.length);
-      done();
-    });
-  });
-
-  it('should return complete client description', function(done) {
-    soap.createClient(test.baseUrl + '/stockquote?wsdl', function(err, client) {
-      assert.ok(!err);
-      var description = client.describe(),
-          expected = { input: { tickerSymbol: "string" }, output:{ price: "float" } };
-      assert.deepEqual(expected , description.StockQuoteService.StockQuotePort.GetLastTradePrice);
-      done();
-    });
-  });
-
-  it('should return correct results', function(done) {
-    soap.createClient(test.baseUrl + '/stockquote?wsdl', function(err, client) {
-      assert.ok(!err);
-      client.GetLastTradePrice({ tickerSymbol: 'AAPL'}, function(err, result) {
-        assert.ok(!err);
-        assert.equal(19.56, parseFloat(result.price));
-        done();
-      });
-    });
-  });
-
-  it('should include response and body in error object', function(done) {
-    soap.createClient(test.baseUrl + '/stockquote?wsdl', function(err, client) {
-      assert.ok(!err);
-      client.GetLastTradePrice({ tickerSymbol: 'trigger error' }, function(err, response, body) {
-        assert.ok(err);
-        assert.strictEqual(err.response, response);
-        assert.strictEqual(err.body, body);
-        done();
-      });
-    });
-  });
-
-// NOTE: this is actually a -client- test
-/*
-it('should return a valid error if the server stops responding': function(done) {
-  soap.createClient(test.baseUrl + '/stockquote?wsdl', function(err, client) {
-    assert.ok(!err);
-    server.close(function() {
-      server = null;
-      client.GetLastTradePrice({ tickerSymbol: 'trigger error' }, function(err, response, body) {
-        assert.ok(err);
-        done();
-      });
-    });
-  });
-});
-*/
-
-});
+        'should return correct results': function(done) {
+            soap.createClient('http://localhost:15099/stockquote?wsdl', function(err, client) {
+                assert.ok(!err);
+                client.GetLastTradePrice({ tickerSymbol: 'AAPL'}, function(err, result) {
+                    assert.ok(!err);
+                    assert.equal(19.56, parseFloat(result.price));
+                    done();
+                });            
+            });
+        }
+    }
+}
